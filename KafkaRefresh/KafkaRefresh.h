f3539c918d263c7cdc8146b0a2cd9180dd35f82a
Hsiang Hsiao-Hui <xorshine@icloud.com>
2020-10-15 06:10:10 +0800

    V1.5.0 🎄

--- KafkaRefresh/KafkaRefresh.h
+++ KafkaRefresh/KafkaRefresh.h
@@ -1,11 +1,11 @@
 /*************************************************************
- * Copyright (c) 2016-present, K.							 *
- * All rights reserved.										 *
- *															 *
- * e-mail: xorshine@icloud.com								 *
- * github:https://github.com/xorshine						 *
- *															 *
- * This source code is licensed under the MIT license.		 *
+ * Copyright (c) 2016-present, K.
+ * All rights reserved.
+ *
+ * e-mail: xorshine@icloud.com
+ * github:https://github.com/xorshine
+ *
+ * This source code is licensed under the MIT license.		 
  *************************************************************/
 
 #if __has_include(<KafkaRefresh/KafkaRefresh.h>)
