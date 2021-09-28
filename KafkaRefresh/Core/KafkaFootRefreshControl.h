5fd2c23b29783d7204ca8dc8546f1cd989ad64a6
H. H. Hsiang <xorshine@icloud.com>
2021-09-28 12:26:53 +0800

    Update KafkaFootRefreshControl.h

--- KafkaRefresh/Core/KafkaFootRefreshControl.h
+++ KafkaRefresh/Core/KafkaFootRefreshControl.h
@@ -2,11 +2,12 @@
  * Copyright (c) 2016-present, K.
  * All rights reserved.
  *
- * e-mail:xorshine@icloud.com
+ * e-mail:xiaohuiprivate@gmail.com
  * github:https://github.com/xorshine
  *
  * This source code is licensed under the MIT license.
  */
+
 
 #import "KafkaRefreshControl.h" 
 
