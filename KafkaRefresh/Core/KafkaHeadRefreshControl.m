fb483855260fcecbdef63ed98d8b15b2a5729e0d
H. H. Hsiang <xorshine@icloud.com>
2021-09-28 12:26:34 +0800

    Update KafkaHeadRefreshControl.m

--- KafkaRefresh/Core/KafkaHeadRefreshControl.m
+++ KafkaRefresh/Core/KafkaHeadRefreshControl.m
@@ -2,7 +2,7 @@
  * Copyright (c) 2016-present, K.
  * All rights reserved.
  *
- * e-mail:xorshine@icloud.com
+ * e-mail:xiaohuiprivate@gmail.com
  * github:https://github.com/xorshine
  *
  * This source code is licensed under the MIT license.
